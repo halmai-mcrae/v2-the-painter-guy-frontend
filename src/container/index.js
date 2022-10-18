7dd3c59840f34c83f2bf6133c7c48dde7b100fea
Halmai McRae <halmaimcrae@Halmais-MacBook-Pro.local>
2022-10-18 13:29:55 +1300

    Set up components

---
+++ src/container/index.js
@@ -0,0 +1,7 @@
+export { default as About } from './About'
+export { default as Footer } from './Footer'
+export { default as Header } from './Header'
+export { default as Services } from './Services'
+export { default as Team } from './Team'
+export { default as Testimonials } from './Testimonials'
+export { default as Work } from './Work'
