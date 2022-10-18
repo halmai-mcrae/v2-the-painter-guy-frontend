594e47df69daed7e0639f92cd915161ceece80c9
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-10-18 19:15:36 +1300

    Add constants for images

--- src/constants/images.js
+++ src/constants/images.js
@@ -26,6 +26,7 @@ import about04 from '../assets/about04.png'
 import profile from '../assets/profile.png'
 import circle from '../assets/circle.svg'
 import logo from '../assets/logo.png'
+import logoWhite from '../assets/logo-white.png'
 
 import adidas from '../assets/adidas.png'
 import amazon from '../assets/amazon.png'
@@ -35,10 +36,14 @@ import nb from '../assets/nb.png'
 import skype from '../assets/skype.png'
 import spotify from '../assets/spotify.png'
 
+//background image
+import bg from '../assets/bgIMG.png'
+
 export default {
   email,
   mobile,
   api,
+  bg,
   cpp,
   css,
   figma,
@@ -62,6 +67,7 @@ export default {
   profile,
   circle,
   logo,
+  logoWhite,
   adidas,
   amazon,
   asus,
