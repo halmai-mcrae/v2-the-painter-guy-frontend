8a56915d04bb7ae11ba61f65dc15357401daccc7
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-10-19 14:10:19 +1300

    Connect sanity to front end

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,60 +1,61 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
-
-import images from '../../constants/images'
 
 import './About.scss'
 
+import { urlFor, client } from '../../client'
+
 const About = () => {
+  const [abouts, setAbouts] = useState([])
 
-  const abouts = [
-    {
-      title: 'Wrestler',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet nisl.',
-      imgUrl: images.about01,
-      imgAlt: 'Wrestler'
-    },
-    {
-      title: 'Khandallah',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet nisl.',
-      imgUrl: images.about02,
-      imgAlt: 'Khandallah'
-    },
-    {
-      title: 'Prefab',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet nisl.',
-      imgUrl: images.about03,
-      imgAlt: 'Prefab'
-    }
-
-  ]
-
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+    client.fetch(query).then((data) => {
+      setAbouts(data)
+    })
+  }, [])
 
   return (
-  
-  <>
-  <span></span>
-  <p className="head-text">Good <span> design</span> <br />is <span>good business.</span></p>
+    <>
+      <span></span>
+      <p className="head-text">
+        Good <span> design</span> <br />
+        is <span>good business.</span>
+      </p>
 
-  <div className="app__profiles">
-    {abouts.map((about, index) => (
-      <motion.div
-      whileInView={ { opacity: 1, scale: 1 } }
-      whileHover={ { scale: 1.1 } }
-      transition={ { duration: 0.5, type: 'tween' } }
-      className="app__profile-item"
-      key={about.title + index}
-      >
-        <img src={about.imgUrl} alt={about.title} />
-        <h3 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h3>
-        <p className='text' style={{ marginTop: 20 }}>{about.title}</p>
-
-    </motion.div>
-    ))}
-</div>
-</>
-);
-};
-
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1, scale: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{
+              duration: 0.5,
+              type: 'tween',
+            }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img
+              src={urlFor(about.imgUrl)}
+              alt={about.title}
+            />
+            <h3
+              className="bold-text"
+              style={{ marginTop: 20 }}
+            >
+              {about.title}
+            </h3>
+            <p
+              className="text"
+              style={{ marginTop: 20 }}
+            >
+              {about.title}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  )
+}
 
 export default About
