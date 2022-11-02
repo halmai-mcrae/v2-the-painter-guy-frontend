2f096428dec99d412a1d8435955618a506642741
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-11-03 11:21:05 +1300

    Change testimonials to slider view

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { motion } from 'framer-motion'
+// import { motion } from 'framer-motion'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
 
 import './Testimonial.scss'
 
@@ -11,7 +12,12 @@ import {
 import { urlFor, client } from '../../client'
 
 const Testimonial = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [tests, setTests] = useState([])
+
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
 
   useEffect(() => {
     const query = '*[_type == "testimonials"]'
@@ -26,40 +32,37 @@ const Testimonial = () => {
       <p className="head-text">
         Testi<span>monials.</span> <br />
       </p>
+      <>
+      {tests.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img src={urlFor(tests[currentIndex].imgUrl)} alt={tests[currentIndex].name} />
+            <div className="app__testimonial-content">
+              <p className="p-text">{tests[currentIndex].feedback}</p>
+              <div>
+                <h4 className="bold-text">{tests[currentIndex].name}</h4>
+                <h5 className="p-text">{tests[currentIndex].company}</h5>
+              </div>
+            </div>
+          </div>
 
-      <div className="app__profiles">
-        {tests.map((test, index) => (
-          <motion.div
-            whileInView={{ opacity: 1, scale: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{
-              duration: 0.5,
-              type: 'tween',
-            }}
-            className="app__profile-item"
-            key={test.name + index}
-          >
-            <img
-              src={urlFor(test.imgUrl)}
-              alt={test.imgUrl}
-            />
-            <h3
-              className="bold-text"
-              style={{ marginTop: 20 }}
-            >
-              {test.name}
-            </h3>
-              
-            <p
-              className="text"
-              style={{ marginTop: 20 }}
-            >{test.feedback}</p>
-          </motion.div>
-        ))}
-      </div>
+          <div className="app__testimonial-btns app__flex">
+            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? tests.length - 1 : currentIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+
+            <div className="app__flex" onClick={() => handleClick(currentIndex === tests.length - 1 ? 0 : currentIndex + 1)}>
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+    </>
     </>
   )
 }
+
+
 export default AppWrap(
   MotionWrap(Testimonial, 'app__about'),
   'testimonials',
