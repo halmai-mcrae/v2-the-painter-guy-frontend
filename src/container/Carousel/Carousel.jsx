5f7c12d220c0848b060aed8f0e0f2f514e53118c
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-11-03 11:20:15 +1300

    Modify carousel for sanity

--- src/container/Carousel/Carousel.jsx
+++ src/container/Carousel/Carousel.jsx
@@ -1,94 +1,46 @@
-import React from 'react'
-
-import Carousel from 'react-bootstrap/Carousel'
+import React, { useEffect, useState } from 'react'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
+import { urlFor, client } from '../../client'
 
 import './Carousel.scss'
 
 const Gallery = () => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [photos, setPhotos] = useState([])
+
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  useEffect(() => {
+    const query = '*[_type == "slider"]'
+    client.fetch(query).then((data) => {
+      setPhotos(data)
+    })
+  }, [])
   return (
-    <div className="carousel">
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="images"
-            src="https://images.unsplash.com/photo-1605201470550-f107ba6fdad3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First</h3>
-            <p>
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="images"
-            src="https://images.unsplash.com/photo-1520420097861-e4959843b682?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-            alt="Second slide"
-          />
 
-          <Carousel.Caption>
-            <h3>Second</h3>
-            <p>
-              Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="images"
-            src="https://images.unsplash.com/photo-1501366062246-723b4d3e4eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1792&q=80"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third</h3>
-            <p>
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="images"
-            src="https://images.unsplash.com/photo-1605201470550-f107ba6fdad3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First</h3>
-            <p>
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="images"
-            src="https://images.unsplash.com/photo-1520420097861-e4959843b682?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-            alt="Second slide"
-          />
+    <>
+      {photos.length && (
+        <>
+          <div className="app__slider-item app__flex">
+            <img src={urlFor(photos[currentIndex].imgUrl)} alt={photos[currentIndex].name} />
 
-          <Carousel.Caption>
-            <h3>Second</h3>
-            <p>
-              Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="images"
-            src="https://images.unsplash.com/photo-1501366062246-723b4d3e4eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1792&q=80"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third</h3>
-            <p>
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        
-      </Carousel>
-    </div>
+          </div>
+
+          <div className="app__slider-btns app__flex">
+            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? photos.length - 1 : currentIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+
+            <div className="app__flex" onClick={() => handleClick(currentIndex === photos.length - 1 ? 0 : currentIndex + 1)}>
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+    </>
   )
 }
 
